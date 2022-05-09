41e4f5cd2238eebd12de3a1914da1a1c50c71871
Henrique Hott <dev.henriquehott@gmail.com>
2022-05-09 08:22:55 -0300

    Cleaning comments.

--- src/img_matrix.c
+++ src/img_matrix.c
@@ -15,13 +15,13 @@ struct img_matrix_t {
 void mimg_SetPixel(int x, int y, pixel p, void * miPtr) {
 	MImage mi = (MImage)(miPtr);
 
-	int offset = (x * mi->width) + y;
+	int index = (x * mi->width) + y;
 
-	mi->matrix[offset].r = p.r;
-	mi->matrix[offset].g = p.g;
-	mi->matrix[offset].b = p.b;
+	mi->matrix[index].r = p.r;
+	mi->matrix[index].g = p.g;
+	mi->matrix[index].b = p.b;
 
-	mi->matrix[offset].li = LI(&p);
+	mi->matrix[index].li = LI(&p);
 }
 
 MImage mimg_Load(const char * filePath) {
@@ -64,130 +64,3 @@ void mimg_Free(MImage mi) {
 	free(mi);
 }
 
-//MImage mimg_Load(const char * filePath) {
-//	FILE * file = fopen(filePath, "r");
-//	if (NULL == file) {
-//		fprintf(stderr, "Could not open file: %s\n", filePath);
-//		exit(EXIT_FAILURE);
-//	}
-//
-//	char line[200];
-//
-//	fgets(line, sizeof(line), file); // Signature, first line.
-//
-//	MImage mi = (MImage) malloc(sizeof(struct img_matrix_t));
-//
-//	// Ignore comments between signature and properties of file.
-//	fgets(line, sizeof(line), file);
-//	while (line[0] == '#') fgets(line, sizeof(line), file);
-//
-//	sscanf(line, "%d %d", &mi->width, &mi->height);
-//
-//	// Ignore comments between properties and max component value.
-//	fgets(line, sizeof(line), file);
-//	while (line[0] == '#') fgets(line, sizeof(line), file);
-//
-//	sscanf(line, "%d", &mi->maxComponentValue);
-//
-//	int maxValueLen = mi->maxComponentValue > 0 ? floor(log10(abs(mi->maxComponentValue))) + 1 : 1;
-//
-//	// Ignore comments between max value and first line with any pixel.
-//	fgets(line, sizeof(line), file);
-//	while (line[0] == '#') fgets(line, sizeof(line), file);
-//
-//	// Allocate pixel matrix.
-//	mi->matrix = (pixel *) malloc((mi->width * mi->height) * sizeof(pixel));
-//
-//	short component = 0;
-//
-//	int x = 0; int y = 0;
-//
-//	char * lineRef = &line[0];
-//
-//	char componentStr[maxValueLen + 1];
-//	componentStr[maxValueLen] = '\0';
-//
-//	int red = -1; int green = -1; int blue = -1;
-//
-//	while (1) {
-//		int lineLength = strlen(lineRef);
-//
-//		short bHasFoundComponent = 0;
-//		for (int i = 0; i < lineLength && !bHasFoundComponent; i++) {
-//			char c = lineRef[i];
-//			int cCode = (int)(c);
-//
-//			if (c == '#') {
-//				bHasFoundComponent = 0;
-//				break;
-//			}
-//
-//			if (cCode >= 48 && cCode <= 57) {
-//				bHasFoundComponent = 1;
-//
-//				int j = 0;
-//				for (; j < maxValueLen; j++) {
-//					char d = lineRef[i + j];
-//					int dCode = (int)(d);
-//
-//					if (!(dCode >= 48 && dCode <= 57)) {
-//						componentStr[j] = '\0';
-//						break;
-//					}
-//
-//					componentStr[j] = d;
-//				}
-//				lineRef = &lineRef[i+j];
-//			}
-//		}
-//
-//		if (!bHasFoundComponent) {
-//			void * OK = fgets(line, sizeof(line), file);
-//
-//			if (OK == NULL) break;
-//
-//			while (line[0] == '#') fgets(line, sizeof(line), file);
-//			lineRef = &line[0];	
-//
-//			continue;
-//		}
-//
-//		int componentValue = atoi(componentStr);
-//
-//		switch (component) {
-//			case 0:
-//				component++;
-//				red = componentValue;
-//			continue;
-//
-//			case 1:
-//				component++;
-//				green = componentValue;
-//			continue;
-//
-//			case 2:
-//				component = 0;
-//				blue = componentValue;
-//
-//				int offset = (x * mi->width) + y;
-//
-//				mi->matrix[offset].r = red;
-//				mi->matrix[offset].g = green;
-//				mi->matrix[offset].b = blue;
-//
-//				y++;
-//			break;
-//		}
-//
-//		if (y == mi->width) {
-//			y = 0;
-//			x++;
-//		}
-//
-//		if ((x == mi->height)) break;
-//	}
-//
-//	fclose(file);
-//
-//	return mi;
-//}
